a1d5e5c2081240637f11312c0d0ef3eb4ff7b2de
xiaowh <932599583@qq.com>
2019-06-05 17:30:50 +0800

    init

---
+++ app/src/androidTest/java/app/android/awesome_module_kotlin/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package app.android.awesome_module_kotlin
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("app.android.awesome_module_kotlin", appContext.packageName)
+    }
+}
---
+++ module_base/src/main/java/app/android/module_base/BaseActivity.kt
@@ -0,0 +1,11 @@
+package app.android.module_base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
